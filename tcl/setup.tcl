4af6ee846ac6d9adc97f90bdf880205db223de7c
Andrew Lukefahr <andrew@lukefahr.org>
2022-10-24 11:33:58 -0400

    made sims run longer by default

--- tcl/setup.tcl
+++ tcl/setup.tcl
@@ -14,13 +14,13 @@ make_wrapper -files [get_files *.bd] -top
 add_files [glob ${SRCDIR}/*/hdl/*_wrapper.v]
 
 # add testbenches
-create_fileset -simset axi4lite_synth 
-add_files -fileset axi4lite_synth [glob ${TESTDIR}/axi4lite_synth/*.sv]
-set_property top axi4lite_synth_tb [get_filesets axi4lite_synth]
+create_fileset -simset sim_axi4lite_synth 
+add_files -fileset sim_axi4lite_synth [glob ${TESTDIR}/axi4lite_synth/*.sv]
+set_property top axi4lite_synth_tb [get_filesets sim_axi4lite_synth]
 
-create_fileset -simset axi4stream_synth 
-add_files -fileset axi4stream_synth [glob ${TESTDIR}/axi4stream_synth/*.sv]
-set_property top axi4stream_synth_tb [get_filesets axi4stream_synth]
+create_fileset -simset sim_axi4stream_synth 
+add_files -fileset sim_axi4stream_synth [glob ${TESTDIR}/axi4stream_synth/*.sv]
+set_property top axi4stream_synth_tb [get_filesets sim_axi4stream_synth]
 
 # set *.sv to SystemVerilog
 set_property file_type SystemVerilog [get_files *.sv]
@@ -30,4 +30,7 @@ current_fileset -simset [ get_filesets axi4lite_synth ]
 
 #set the top for synthesis
 set_property top bd_fpga_wrapper [current_fileset]
+
+#make sims run longer by default
+set_property -name {xsim.simulate.runtime} -value {1000us} -objects [get_filesets sim_*]
 
